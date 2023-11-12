e28090316ed7fd5df1fd9fe1466937e1e514a5e3
duberii <108533494+duberii@users.noreply.github.com>
2023-11-12 11:54:30 -0500

    Update cloud_chamber.js
    Comments, simplifications, and graphics change

--- cloud_chamber.js
+++ cloud_chamber.js
@@ -1,101 +1,105 @@
 
 // Simulation Parameters
 const particlesize = 4;
-const minvelocity = 0.01;
+const minvelocity = 0.2;
 const decayRate = 0.004;
 const numParticles = 20;
 const spawnChance = 0.95;
-const fadeRate = 3;
+const fadeRate = 20;
 const fieldStrength = 0.01;
 const slowDown = 0.99;
+const trailSize = 4;
 
 //Particle Lists
 let particles = [];
-let fadingParticles = [];
 let toRemove = [];
 
 function setup() {
     let cnv = createCanvas(windowWidth, windowHeight);
     cnv.parent('bg');
     cnv.position(0, 0);
-    cnv.style('z-index', '-10'); 
-    cnv.style('position','fixed') 
+    cnv.style('z-index', '-10');
+    cnv.style('position', 'fixed')
+    background(34, 35, 52);
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-  }
+}
+function mouseClicked() {
+    background(34, 35, 52);
+}
 function draw() {
-    background('#222334');
+    background(34, 35, 52, fadeRate);
     // Create new particles occasionally if there are under numParticles
-    if (particles.length + fadingParticles.length < numParticles & Math.random() > spawnChance) {
+    if (particles.length < numParticles & Math.random() > spawnChance) {
         particles.push(new Particle());
     }
     // Make particles fade if they are outside of the screen or if their velocity is below minvelocity
     for (let i = particles.length - 1; i >= 0; i--) {
-        if (particles[i].position.x < 0 || particles[i].position.x > width || particles[i].position.y < 0 || particles[i] >
-            height || particles[i].velocity.mag() < minvelocity) { toRemove.push(i); }
+        if (particles[i].position.x < 0 || particles[i].position.x > width || particles[i].position.y < 0 || particles[i].position.y >
+            height || particles[i].velocity.mag() < minvelocity) {
+            particles.splice(i, 1);
+            continue
+        }
         // The update method returns a bool stating whether or not the particle decayed let 
         didDecay = particles[i].update();
         if (didDecay) {
             // Generates a random velocity, and calculates the original 
-            let temp = p5.Vector.random2D(); 
+            let temp = p5.Vector.random2D();
             let originalCharge = particles[i].charge;
             let originalMomentum = p5.Vector.mult(particles[i].velocity.copy(), particles[i].mass);
             if (!(particles[i].charge == 0)) {
+                //For charged particles, a random charge value is chosen for the daughter particles. The value is generated so that 
+                // the decaying particle can be used as a daughter particle as well
                 particles[i].charge = 2 * getRandomInt(0, 1) - 1;
                 particles[i].mass = random() + 1;
                 particles[i].velocity = temp.copy();
                 particles[i].decayProbability = particles[i].decayProbability / 2;
                 particles[i].acceleration = particles[i].calc_acceleration();
+
+                // The other particle is generated based on conservation of momentum and conservation of charge
                 particles.push(new Particle());
                 particles[particles.length - 1].position = particles[i].position.copy();
                 particles[particles.length - 1].velocity = p5.Vector.mult(p5.Vector.sub(originalMomentum.copy(), p5.Vector.mult(particles[i].velocity.copy(), particles[i].mass)), 1 / particles[particles.length - 1].mass);
                 particles[particles.length - 1].charge = originalCharge - particles[i].charge;
                 particles[particles.length - 1].decayProbability = particles[i].decayProbability / 2;
                 particles[particles.length - 1].acceleration = particles[particles.length - 1].calc_acceleration();
-                if (particles[particles.length - 1].charge == 0) {
-                    particles[particles.length - 1].toDraw = false;
-                }
             } else {
-                toRemove.push(i)
-                particles[i].fading = true;
+                // For neutral particles, the decays always contain one positive particle and one negative particle
                 particles.push(new Particle());
                 particles[particles.length - 1].position = particles[i].position.copy();
                 particles[particles.length - 1].velocity = temp.copy();
                 particles[particles.length - 1].charge = -1;
-                particles[particles.length - 1].toDraw = true;
                 particles[particles.length - 1].decayProbability = particles[i].decayProbability / 4;
                 particles[particles.length - 1].acceleration = particles[particles.length - 1].calc_acceleration();
                 particles.push(new Particle());
                 particles[particles.length - 1].position = particles[i].position.copy();
                 particles[particles.length - 1].velocity = p5.Vector.mult(p5.Vector.sub(originalMomentum.copy(), p5.Vector.mult(particles[particles.length - 2].velocity.copy(), particles[i].mass)), 1 / particles[particles.length - 1].mass);
-                particles[particles.length - 1].charge = 1; particles[particles.length - 1].toDraw = true;
+                particles[particles.length - 1].charge = 1;
                 particles[particles.length - 1].decayProbability = particles[i].decayProbability / 4;
                 particles[particles.length - 1].acceleration = particles[particles.length - 1].calc_acceleration();
+                particles.splice(i, 1);
             }
         }
-    } for (let i = toRemove.length - 1; i >= 0; i--) {
-        fadingParticles.push(particles.splice(i, 1)[0]);
-        fadingParticles[fadingParticles.length - 1].fading = true;
     }
-    for (let i = fadingParticles.length - 1; i >= 0; i--) {
-        fadingParticles[i].update();
-        let isDone = fadingParticles[i].display();
-        if (isDone) {
-            fadingParticles.splice(i, 1);
-        }
-    }
-    toRemove = [];
     for (let i = particles.length - 1; i >= 0; i--) {
+        //Displays all of the particles
         particles[i].display();
     }
 }
 
 class Particle {
     constructor() {
+        // randInt randomly picks which side of the screen the particle will enter from
         let randInt = getRandomInt(0, 3);
+
+        //tempLen is the magnitude of the velcity vector
         let tempLen = 5 * random() + 3;
+
+        //Angle is the angle of the velocity from the inward normal from the edge of the screen
         let angle = 1.6 * random() - 0.8;
+
+        //Based on the side of the screen to enter from, a random entry point is chosen and the velocity is calculated as a vector
         if (randInt == 0) {
             this.position = createVector(0.8 * width * random() + 0.2 * width, 0);
             this.velocity = createVector(tempLen * Math.sin(angle), tempLen * Math.cos(angle));
@@ -111,73 +115,56 @@ class Particle {
             this.velocity = createVector(-1 * tempLen * Math.cos(angle), tempLen * Math.sin(angle));
         }
 
-        this.charge = getRandomInt(-1, 1); // Adjust charge values as needed
-        this.mass = random() + 0.1;
+        //Randomly generates properties of the particle
+        this.charge = getRandomInt(-1, 1); 
+        this.mass = random() + 0.5;
         this.decayProbability = decayRate;
-        this.trailColor = color(255, 150); // Adjust trail color and opacity
         this.acceleration = this.calc_acceleration();
         this.history = [];
-        this.fading = false;
-        this.fadingCount = 0;
-        this.guaranteed_decay = 30 * random() + 10;
-        if (this.charge == 0) {
-            this.toDraw = false;
-        } else {
-            this.toDraw = true;
-        }
+
+        //Neutral particles are guaranteed to decay after a certain number of ticks. This value determines 
+        //how many ticks need to pass before the decay
+        this.guaranteed_decay = 50 * random() + 10;
     }
 
     update() {
-        // Slow down over time
+        // Adds the current position to the history list for use in drawing the trail
         this.history.push(createVector(this.position.x, this.position.y));
-        if (this.history.length > 300) {
+
+        // Deletes history if it is too long
+        if (this.history.length > 100) {
             this.history.shift();
         }
+        // Particles slow down over time
         this.velocity.mult(slowDown);
         this.acceleration = this.calc_acceleration();
         this.velocity.add(this.acceleration);
         // Move the particle
         this.position.add(this.velocity);
-        // Decay and create new particles occasionally
-        let distance_from_spawn = p5.Vector.sub(this.history[0].copy(), this.position.copy()).mag();
-        if (random() < this.decayProbability & !(this.fading) & this.velocity.mag() > minvelocity & !(this.charge == 0)) {
+        // Checks for certain decay conditions, and returns a bool saying if the particle decays or not
+        if (random() < this.decayProbability & this.velocity.mag() > minvelocity & !(this.charge == 0)) {
             return true;
-        } else if (distance_from_spawn > this.guaranteed_decay & this.charge == 0 & !(this.fading)) {
+        } else if (this.history.length > this.guaranteed_decay & this.charge == 0) {
             return true;
         } else {
             return false;
         }
     }
     calc_acceleration() {
+        // Calculates acceleration based on the Lorentz force
         return createVector((-1 * fieldStrength / this.mass) * this.charge * this.velocity.y, (fieldStrength / this.mass) * this.charge * this.velocity.x);
     }
     display() {
-        if (this.fading) {
-            this.fadingCount += 1;
-        }
-        if (this.toDraw) {
-            let opac = 255;
-            if (this.fading) {
-                opac = 255 - fadeRate * this.fadingCount;
-                if (opac <= 1) { opac = 0; }
-            } for (let i = this.history.length - 1; i > 0; i--) {
-                let trailSize = particlesize;
-                this.trailColor.setAlpha(map(i, this.history.length, 0, 0, opac));
-                strokeWeight(trailSize);
-                stroke(this.trailColor);
-                line(this.history[this.history.length - i].x, this.history[this.history.length - i].y, this.history[this.history.length - i - 1].x, this.history[this.history.length - i - 1].y);
+        //Neutral particles are not drawn
+        if (!(this.charge == 0)) {
+            strokeWeight(trailSize);
+            stroke(color(255));
+            noFill();
+            beginShape();
+            for (let i = 1; i <=this.history.length - 1; i++) {
+                curveVertex(this.history[this.history.length - i].x, this.history[this.history.length - i].y);
             }
-            // Draw the particle based on its charge
-            if (!(this.fading)) {
-                fill(255, 255, 255, 255);
-                noStroke();
-                ellipse(this.position.x, this.position.y, particlesize, particlesize);
-            }
-        }
-        if (this.fadingCount >= 130) {
-            return true;
-        } else {
-            return false;
+            endShape();
         }
     }
 
